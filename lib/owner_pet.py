e95919b486193140bd2c9fc8e642e5d3a7b0960e
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-15 08:22:27 +0300

    feat: add add_pet method to Owner with type checking and owner assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -17,3 +17,8 @@ class Owner:
 
     def pets(self):
         return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("pet must be an instance of Pet")
+        pet.owner = self
