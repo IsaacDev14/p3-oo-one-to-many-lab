aa9c43dbd4a926c33329d737e61a760fc35ce619
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-15 08:15:50 +0300

    feat: define Pet class with name, pet_type, and optional owner

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -2,8 +2,10 @@ PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
 
 class Pet:
     all = []
-    def __init__(self, pet_type ):
+    def __init__(self, name, pet_type, owner = None ):
+        self.name = name
         self.pet_type = pet_type
+        self.owner = owner
 
 class Owner:
     def __init__(self, name):
